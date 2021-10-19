d6e5009860c00e2168b8560e1e1f426231762c4e
Bandhitawkunthi <92640904+Bandhitawkunthi@users.noreply.github.com>
2021-10-19 23:21:22 +0700

    Add files via upload

---
+++ calcu.py
@@ -0,0 +1,30 @@
+#code By Bandhitawkunthi
+while True:
+		print("===================")
+		print("===================")
+		print("  ")
+		print("ＳＩＭＰＬＥ ＣＡＬＣＵＬＡＴＯＲ")
+		print("Code by Bandhitawkunthi")
+		print("  ")
+		print("===================")
+		print("===================")
+		print("Enter the operation you want, for example +,-,*,/,")
+		
+		operasi = input("{?} Enter Your Operation : ")
+		if operasi not in("+","-","*","/"):
+			print("Closed connetions.....")
+			break
+		
+		
+		a = int(input("First number : "))
+		b = int(input("Second number : "))
+		
+		if operasi == "+":
+		    print("Result of",a ,"+",b ,"=",a+b)
+		elif operasi == "-":
+		   	print("Result of",a ,"-",b ,"=",a-b)
+		elif operasi == "*":
+			   print("Result of",a ,"*",b ,"=",a*b)
+		elif operasi == "/":
+		   	print("Result of",a , "/",b ,"=",a/b)
+		
